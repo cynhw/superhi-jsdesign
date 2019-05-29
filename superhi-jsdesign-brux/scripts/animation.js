bb8b16c23d08f8c947593a0e5c6d575f67432825
cynhw <cynhowong@gmail.com>
2019-05-28 22:52:37 -0700

    finished code for fade in and out

---
+++ superhi-jsdesign-brux/scripts/animation.js
@@ -0,0 +1,41 @@
+const animatedTags = document.querySelectorAll("h2, h3, p, section img, a.button");
+
+// fade out on load
+
+animatedTags.forEach(tag => {
+  tag.style.opacity = 0;
+});
+
+const fadeIn = function() {
+
+  // look through all the animated tags and see with the 
+  // getBoundingClientRect() if it's in the window
+
+  let delay = 0.25;
+
+  animatedTags.forEach(tag => {
+    const tagTop = tag.getBoundingClientRect().top;
+    const tagBottom = tag.getBoundingClientRect().bottom;
+
+    if (tagTop < window.innerHeight && tagBottom > 0) {
+      tag.style.animation = `fadeIn 1s ${delay}s both`;
+      delay = delay + 0.25;
+    } else {
+      tag.style.opacity = 0;
+      tag.style.animation = '';
+    };
+
+  });
+};
+
+fadeIn();
+
+document.addEventListener("scroll", function() {
+  fadeIn();
+});
+
+// on browser resize, run fadeIn
+
+window.addEventListener("resize", function() {
+  fadeIn();
+})
